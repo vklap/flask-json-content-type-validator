967c3fadc384a9e540a74c6341cf64b403267055
Victor Klapholz <victor@stuccomedia.com>
2017-12-29 21:57:44 +0200

    add long description for pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='flask_json_content_type_validator',
-    packages=['flask_json_content_type_validator'],
+    packages=find_packages(),
     version='0.0.1',
     description='Flask JSON Content-Type decorator which validates that the '
     'Content-Type is indeed application/json. In the case of a wrong content '
     'type, then the decorator will throw an exception which can be customized.',
+    long_description=open('README.md').read(),
     author='Victor Klapholz',
     author_email='victor.klapholz@gmail.com',
     url='https://github.com/vklap/flask-json-content-type-validator',
