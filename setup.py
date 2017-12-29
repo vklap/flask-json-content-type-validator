76aabc54345796414fbb90b263945870bbf6ade1
Victor Klapholz <victor@stuccomedia.com>
2017-12-29 23:23:15 +0200

    version 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='flask_json_content_type_validator',
     packages=find_packages(),
-    version='0.0.1',
+    version='0.1.0',
     description='Flask JSON Content-Type decorator which validates that the '
     'Content-Type is indeed application/json. In the case of a wrong content '
     'type, then the decorator will throw an exception which can be customized.',
@@ -12,6 +12,7 @@ setup(
     author_email='victor.klapholz@gmail.com',
     url='https://github.com/vklap/flask-json-content-type-validator',
     keywords='flask json content validation decorator',
+    license='MIT',
     install_requires=[
         "Flask==0.12.2"
     ],
