738b4a9292a89309f131bba8aac80f387fb62e00
Victor Klapholz <victor@stuccomedia.com>
2017-12-29 21:45:16 +0200

    push to make travis-ci run for first time

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='flask_json_content_type_validator',
     packages=['flask_json_content_type_validator'],
     version='0.0.1',
-    description='Flask JSON Content-Type decorator which validates that the'
-    'Content-Type is indeed application/json. In the case of a wrong content'
+    description='Flask JSON Content-Type decorator which validates that the '
+    'Content-Type is indeed application/json. In the case of a wrong content '
     'type, then the decorator will throw an exception which can be customized.',
     author='Victor Klapholz',
     author_email='victor.klapholz@gmail.com',
